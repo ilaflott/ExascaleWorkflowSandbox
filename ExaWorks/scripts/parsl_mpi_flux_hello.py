008725fa428711669d0b292d46bfe1abcaef2cb9
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2023-05-30 19:53:17 +0000

    Cleanup/document parsl/flux hello test program

--- ExaWorks/scripts/parsl_mpi_flux_hello.py
+++ ExaWorks/scripts/parsl_mpi_flux_hello.py
@@ -7,7 +7,6 @@ from parsl.channels import LocalChannel
 from parsl.providers import SlurmProvider
 from parsl.executors import FluxExecutor
 from parsl.launchers import SimpleLauncher
-from parsl.launchers import SrunLauncher
 from parsl.addresses import address_by_hostname
 from parsl.data_provider.files import File
 
@@ -16,16 +15,18 @@ config = Config(
     executors=[
         FluxExecutor(
             label="hera_flux",
+            # Start Flux with srun and tell it there are 40 cores per node
             launch_cmd='srun --tasks-per-node=1 -c40 ' + FluxExecutor.DEFAULT_LAUNCH_CMD,
             provider=SlurmProvider(
                 channel=LocalChannel(),
-                nodes_per_block=20,
+                nodes_per_block=2,
                 init_blocks=1,
                 partition='hera',
                 account='gsd-hpcs',
                 walltime='00:10:00',
                 launcher=SimpleLauncher(),
                 worker_init='''
+# FluxExecutor ignores worker_init
 ''',
             ),
         )
@@ -40,12 +41,14 @@ parsl.load(config)
 remote = False
 shared_dir = '/scratch2/BMC/gsd-hpcs/Christopher.W.Harrop/SENA/ExascaleWorkflowSandbox/ExaWorks/scripts'
 
+# Print out resources that Flux sees after it starts
 @bash_app
 def resource_list():
     return '''
     flux resource list > resource_list.txt
     '''
 
+# Compile the hello MPI program with Intel
 @bash_app
 def compile_app(dirpath, stdout=None, stderr=None, compiler="mpiifort -fc=ifx", parsl_resource_specification={"num_tasks": 1}):
     return '''
@@ -54,10 +57,12 @@ def compile_app(dirpath, stdout=None, stderr=None, compiler="mpiifort -fc=ifx", 
     {} -o mpi_hello.exe mpi_hello.f90
     '''.format(dirpath, compiler)
 
+# Run the hello MPI program with Intel
 @bash_app
 def mpi_hello(dirpath, stdout=None, stderr=None, app="./mpi_hello.exe", parsl_resource_specification={}):
     return '''
-    module load intel/2022.3.0 impi/2022.3.0    
+    module load intel/2022.3.0 impi/2022.3.0
+    # Flux requires I_MPI_PMI_LIBRARY to be unset because it provides its own PMI
     unset I_MPI_PMI_LIBRARY
     cd {}
     {}
@@ -76,7 +81,7 @@ compile_app(dirpath=shared_dir,
 hello = mpi_hello(dirpath=shared_dir,
                   stdout=os.path.join(shared_dir, "mpi_apps.hello.out"),
                   stderr=os.path.join(shared_dir, "mpi_apps.hello.err",),
-                  parsl_resource_specification={"num_tasks": 800, "num_nodes": 20})
+                  parsl_resource_specification={"num_tasks": 80, "num_nodes": 2})
 
 # Wait for the MPI app to finish
 hello.result()
